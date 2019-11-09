ceef98fe11bcc2f12d573e1f51e5f3b508559e24
Hannatassja <Hannatassja@gmail.com>
2019-11-09 14:29:08 +0700

    added dependencies

---
+++ app/src/main/java/umn/ac/vorgoprojek/Feature_Project/Feature_Projek_1.java
@@ -0,0 +1,14 @@
+package umn.ac.vorgoprojek.Feature_Project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Feature_Projek_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feature__projek_1);
+    }
+}
