da033e4b153cfbcb654df43f34ebfb6f9c6dcde6
Steven Johan <steven.johan@student.umn.ac.id>
2019-11-30 05:18:12 +0700

    Added database

---
+++ app/src/main/java/umn/ac/vorgoprojek/Feature_Project/project.java
@@ -0,0 +1,29 @@
+package umn.ac.vorgoprojek.Feature_Project;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+public class project {
+
+    String edtTitle, color;
+    public project() {
+
+    }
+
+    public String getEdtTitle() {
+        return edtTitle;
+    }
+
+    public void setEdtTitle(String edtTitle) {
+        this.edtTitle = edtTitle;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
