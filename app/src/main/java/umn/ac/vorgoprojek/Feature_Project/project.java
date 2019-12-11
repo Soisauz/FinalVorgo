16a6ce35167e8f193d18421a00c27fb6716186fd
Kemal R Jenie <kemal.jenie@gmail.com>
2019-12-11 19:14:44 +0700

    Add Project Changes

--- app/src/main/java/umn/ac/vorgoprojek/Feature_Project/project.java
+++ app/src/main/java/umn/ac/vorgoprojek/Feature_Project/project.java
@@ -3,6 +3,9 @@ package umn.ac.vorgoprojek.Feature_Project;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class project {
 
     String edtTitle, color;
@@ -24,6 +27,16 @@ public class project {
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    public project(String edtTitle) {
+        this.edtTitle = edtTitle;
+    }
+
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("edtTitle", edtTitle);
+        return result;
     }
 
 }
