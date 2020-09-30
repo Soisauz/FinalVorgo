9311bf1d223ed836da3c89ec3f18b7105883b4b3
stevenjhnlol <55408814+stevenjhnlol@users.noreply.github.com>
2020-09-30 22:48:20 +0700

    Minor FIx

--- app/src/main/java/umn/ac/vorgoprojek/Feature_MyTask/daily.java
+++ app/src/main/java/umn/ac/vorgoprojek/Feature_MyTask/daily.java
@@ -26,7 +26,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
